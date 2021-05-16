82a3f99981eb5685aa5c5de797cf04c38d753cba
nicklausliu <nicklausliu@tencent.com>
2021-05-16 17:20:55 +0800

    feat:修改登录页面样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,63 +1,59 @@
 <template>
-  <div class="login-container" style="background-image: url(./src/assets/img/playstation-pattern.png)">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+  <div class="login-container">
+    <div>
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
-      <div class="title-container">
-        <h2 class="title">环境应急管理系统</h2>
-      </div>
+        <div class="title-container">
+          <h2 class="title">环境应急管理系统</h2>
+        </div>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user"/>
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
+        <el-form-item prop="username">
+          <span class="svg-container">
+            <svg-icon icon-class="user"/>
+          </span>
+          <el-input
+            ref="username"
+            v-model="loginForm.username"
+            placeholder="请输入账号"
+            name="username"
+            type="text"
+            tabindex="1"
+            auto-complete="off"
+          />
+        </el-form-item>
 
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password"/>
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"/>
-        </span>
-      </el-form-item>
+        <el-form-item prop="password">
+          <span class="svg-container">
+            <svg-icon icon-class="password"/>
+          </span>
+          <el-input
+            :key="passwordType"
+            ref="password"
+            v-model="loginForm.password"
+            :type="passwordType"
+            placeholder="请输入密码"
+            name="password"
+            tabindex="2"
+            auto-complete="on"
+            @keyup.enter.native="handleLogin"
+          />
+          <span class="show-pwd" @click="showPwd">
+            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"/>
+          </span>
+        </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+        <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
-      <div class="tips">
-        <span style="margin-right:20px;">
-          超级管理员账号: admin
-          <br>
-          工作人员账号: worker
-          <br>
-          指挥人员账号: monitor
-          <br>
-          专家人员: expert
-          <br>
-          密码: 不低于6位即可
-        </span>
-      </div>
+        <div class="tips">
+          <span style="margin-right:20px;">
+            超级管理员账号: admin | 工作人员账号: worker
+            <br>
+            指挥人员账号: monitor | 专家人员: expert
+          </span>
+        </div>
 
-    </el-form>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -69,14 +65,14 @@ export default {
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('请确认账号是否合法'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('密码长度大于6即可'))
       } else {
         callback()
       }
@@ -164,11 +160,6 @@ $cursor: #fff;
       color: $light_gray;
       height: 47px;
       caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
     }
   }
 
@@ -189,8 +180,11 @@ $light_gray: #eee;
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-color: $bg;
-  overflow: hidden;
+  background-image: url("~@/assets/img/bg_image.png");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  background-attachment: fixed;
+  opacity: 0.8;
 
   .login-form {
     position: relative;
@@ -199,18 +193,27 @@ $light_gray: #eee;
     padding: 250px 35px 0;
     margin: 0 auto;
     overflow: hidden;
+    opacity: revert;
   }
 
   .tips {
     font-size: 14px;
-    color: #fff;
+    color: black;
     margin-bottom: 10px;
+    line-height: 20px;
+    font-weight: bold;
+    font-family: "Microsoft YaHei UI Light";
 
     span {
       &:first-of-type {
         margin-right: 16px;
       }
     }
+  }
+
+  .drag{
+    position: relative;
+    text-align: center;
   }
 
   .svg-container {
@@ -226,10 +229,11 @@ $light_gray: #eee;
 
     .title {
       font-size: 26px;
-      color: $light_gray;
+      color: black;
       margin: 0px auto 40px auto;
       text-align: center;
       font-weight: bold;
+      font-family: "Microsoft YaHei UI Light";
     }
   }
 
